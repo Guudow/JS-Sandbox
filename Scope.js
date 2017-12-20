8d4dd2e1ff9994fef93ea7eea306ee7a436ed732
Guudow <mguudow@aol.com>
2017-12-20 21:33:55 +0100

    Changes to be committed:

---
+++ Scope.js
@@ -0,0 +1,26 @@
+// Global scope
+var a = 1; 
+let b = 2;
+const c = 3;
+
+// function test(){
+//   var a = 4; 
+// let b = 5;
+// const c = 6;
+// console.log('function Scope: ', a, b, c);
+// }
+
+// test();
+
+// if(true){
+//   var a = 4; 
+// let b = 5;
+// const c = 6;
+// console.log('If Scope: ', a, b, c);
+// }
+
+for(let a = 0; a < 10; a++ ){
+  console.log(`loop: ${a}`);
+}
+
+console.log('Global Scope: ', a, b, c);
---
+++ WindowObject.js
@@ -0,0 +1,58 @@
+// Window method / Objects / Properties
+
+// prompt
+// const input = prompt();
+// alert (input);
+
+// Confirm
+// if(confirm('Are you sure')){
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+let val;
+
+// Outter height and Width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// location object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// redirect
+// window.location.href = 'http://google.com';
+
+// // reload
+// window.location.reload();
+
+// history object 
+// window.history.go(-1);
+
+// window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+console.log(val);
+
+
+
